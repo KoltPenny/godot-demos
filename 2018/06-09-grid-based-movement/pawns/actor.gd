eaaac44d933d464cd48b951ac2ab5e76ecfa004f
KoltPenny <kolt.penny@gmail.com>
2020-07-22 09:11:38 -0500

    Removed the need for a Pivot.
    This change shows that there's no need to have a Pivot involved when you can move the Node2D that contains the logic. The cell in which the Actor is to be positioned, is already updated by the Grid. Therefore, it doesn't matter if the position is updated before or after the Tween starts.
    
    This works without issues in Godot 3.2.2.

--- 2018/06-09-grid-based-movement/pawns/actor.gd
+++ 2018/06-09-grid-based-movement/pawns/actor.gd
@@ -30,17 +30,21 @@ func move_to(target_position):
 	set_process(false)
 	$AnimationPlayer.play("walk")
 
-	# Move the node to the target cell instantly,
-	# and animate the sprite moving from the start to the target cell
+	# and animate the sprite while moving from the start to the target cell
 	var move_direction = (target_position - position).normalized()
-	$Tween.interpolate_property($Pivot, "position", - move_direction * 32, Vector2(), $AnimationPlayer.current_animation_length, Tween.TRANS_LINEAR, Tween.EASE_IN)
-	position = target_position
+	$Tween.interpolate_property(
+		self,"position",
+		position,target_position,
+		$AnimationPlayer.current_animation_length,
+		Tween.TRANS_LINEAR, Tween.EASE_IN)
 
 	$Tween.start()
 
 	# Stop the function execution until the animation finished
 	yield($AnimationPlayer, "animation_finished")
 	
+	#Updates the position of the Node after the animation stops.
+	position = target_position
 	set_process(true)
 
 
